49172a70bdacf776a4f3cb93ae129bbcd5f1aaf5
chococrok <moi_viking@hotmail.com>
2017-12-21 01:59:32 +0100

    deleted useless dependencies

--- project_4_web_app/library_webapp_controller/src/main/java/io/ab/library/webapp/action/LibraryActionSupport.java
+++ project_4_web_app/library_webapp_controller/src/main/java/io/ab/library/webapp/action/LibraryActionSupport.java
@@ -2,22 +2,17 @@ package io.ab.library.webapp.action;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-import io.ab.library.webapp.client.impl.AbstractClientSoap;
 import io.ab.library.webapp.service.AuthorService;
 import io.ab.library.webapp.service.BookService;
 import io.ab.library.webapp.wsdl.Author;
 import io.ab.library.webapp.wsdl.Book;
 
 public abstract class LibraryActionSupport extends ActionSupport {
-	
-	protected static final Logger log = LoggerFactory.getLogger(AbstractClientSoap.class);
-	
+		
 	protected List<Author> authors;
 	protected List<Book> books;
 	
--- project_4_web_app/library_webapp_service/src/main/java/io/ab/library/webapp/service/AbstractService.java
+++ project_4_web_app/library_webapp_service/src/main/java/io/ab/library/webapp/service/AbstractService.java
@@ -1,7 +1,5 @@
 package io.ab.library.webapp.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import io.ab.library.webapp.client.AuthorClient;
@@ -14,6 +12,4 @@ public class AbstractService {
 	@Autowired
 	protected BookClient bookClient;
 	
-	protected static final Logger log = LoggerFactory.getLogger(AbstractService.class);
-
 }
