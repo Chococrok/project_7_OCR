3af29a83fa6ed1fe55c05d7a1f4fc34d2c2c8129
chococrok <moi_viking@hotmail.com>
2018-07-05 23:15:34 +0200

    added a reservation number associated to a sequence to be able to get the odest reservation on a book

--- data_base/script/migration/create_reservation_table.sql
+++ data_base/script/migration/create_reservation_table.sql
@@ -1,9 +1,15 @@
+CREATE SEQUENCE public.reservation_traker;
+
+
 CREATE TABLE public.reservation (
                 account_id INTEGER NOT NULL,
                 book_id INTEGER NOT NULL,
                 reservation_end DATE,
+                reservation_number INTEGER NOT NULL DEFAULT nextval('public.reservation_traker'),
                 CONSTRAINT reservation_pk PRIMARY KEY (account_id, book_id)
 );
+
+ALTER SEQUENCE public.reservation_traker OWNED BY public.reservation.reservation_number;
 
 ALTER TABLE public.reservation ADD CONSTRAINT book_reservation_fk
 FOREIGN KEY (book_id)
